2caa72b3fb7232acf35b4555ef4d1e643e11ba67
liu785625446 <785625446@qq.com>
2015-03-17 17:44:22 +0800

    2015031704
    更新

--- DigitalSchool/AppDelegate.m
+++ DigitalSchool/AppDelegate.m
@@ -9,7 +9,8 @@
 #import "AppDelegate.h"
 #import "BLTool.h"
 #import "UMFeedback.h"
-
+#import "PLUserProcess.h"
+#import "PLUser.h"
 #define UMENT_APPKEY @"54fbf719fd98c5ba19000928"
 
 @implementation AppDelegate
@@ -36,6 +37,40 @@
     [@"aa" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     //    [[UINavigationBar appearance] setBackgroundColor:[UIColor redColor]];
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithHexString:@"06AC41" alpha:0.5]];
+    
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    id username = [userDefault objectForKey:REMEMBER_USERNAME];
+    id pwd = [userDefault objectForKey:REMEMBER_PASSWORD];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+       
+        if (username && pwd) {
+            PLUserProcess *userProcess = [[PLUserProcess alloc] init];
+            [userProcess loginUserName:username didPassword:pwd didSuccess:^(NSMutableArray *array) {
+                if (array) {
+                    id data = [array objectAtIndex:0];
+                    if ([data isKindOfClass:[NSDictionary class]]) {
+                        
+                    }else{
+                        [userDefault removeObjectForKey:CURRENT_USER];
+                        [userDefault synchronize];
+                    }
+                }else
+                {
+                    [userDefault removeObjectForKey:CURRENT_USER];
+                    [userDefault synchronize];
+                }
+            } didFail:^(NSString *error) {
+                [userDefault removeObjectForKey:CURRENT_USER];
+                [userDefault synchronize];
+            }];
+        }else{
+            [userDefault removeObjectForKey:CURRENT_USER];
+            [userDefault synchronize];
+        }
+        
+    });
+    
     //    [[UINavigationBar appearance] setTintColor:[UIColor redColor]];
     return YES;
 }
